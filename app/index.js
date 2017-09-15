4e4974d12d9fac00c5646cea90098d6b6ae49e9a
Srikanth Vinukonda <srikanthchandu.v@gmail.com>
2017-09-14 19:10:36 -0500

    initializing dir

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+import css from './style.css'
+import App from './components/App.js'
+
+ReactDOM.render(<App />, document.getElementById('app'));
